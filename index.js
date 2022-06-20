5d13c177daec3a49c0bf440fb1418c8a774b672c
weijie921 <goldrabbit112@gmail.com>
2022-06-20 00:05:23 -0300

    fixed with node js input

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const readline = require('readline-sync');
 let num = Number(readline.question());
 
 let number;
+let results = [];
 for (let i = 0; i < num; ++i) {
   number = [];
   number = readline.question().split(" ").map(a=>Number(a));
@@ -29,12 +30,17 @@ for (let i = 0; i < num; ++i) {
   	if(C > B && A == C && C - 1 == B) rlt = Math.min(rlt, 2);
   	if(C > B && A == C && C - 1 > B) rlt = Math.min(rlt, 1);
   	
-  	if(A == C){
-  		if(C > 2) rlt = Math.min(rlt, B-C + 3);
-  	}else{
-  		if(C > 1) rlt = Math.min(rlt, B-C + 1);
+  	if(C <= B){
+  		if(A == C){
+  			if(C > 2) rlt = Math.min(rlt, B-C + 3);
+	  	}else{
+	  		if(C > 1) rlt = Math.min(rlt, B-C + 1);
+	  	}
   	}
+  	
   }
   if(rlt == Number.MAX_VALUE) rlt = -1;
   console.log(rlt);
+  results.push(rlt);
 }
+console.log(results);
